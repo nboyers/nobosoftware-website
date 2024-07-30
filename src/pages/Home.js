b285dca7eb3920ef1b652050a1dbad5193dad7b0
nboyers <frenttv@gmail.com>
2024-07-30 13:04:10 -0400

    Added Services Button back to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Home.css';
 
 const Home = () => {
@@ -7,6 +8,9 @@ const Home = () => {
       <div className="hero-content">
         <h1>Nobos Software LLC</h1>
         <p>Specializing in the development of iOS and Android applications, with robust cloud hosting solutions.</p>
+        <Link to="/services">
+          <button className="services-button">VIEW SERVICES</button>
+        </Link>
       </div>
     </div>
   );
