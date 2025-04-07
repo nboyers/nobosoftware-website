7c76f9a0c9a5a20fddcfd3eb6a2e4876cdc3e962
Noah Boyers <hello@nobosoftware.com>
2025-04-06 21:31:38 -0400

    Footer Updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
 const Footer = () => {
   return (
-    <footer className="App-footer">
-      <p>&copy; 2025 Nobos Software LLC. All rights reserved.</p>
+    <footer className="site-footer">
+      <p>© {new Date().getFullYear()} Nobos Software LLC. All rights reserved.</p>
+      <div className="footer-links">
+        <Link to="/privacy-policy">Privacy Policy</Link>
+        <span>&nbsp;|&nbsp;</span>
+        <Link to="/tos">Terms of Service</Link>
+      </div>
     </footer>
   );
 };
