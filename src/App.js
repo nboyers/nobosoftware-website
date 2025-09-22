3d8c4cf2f305adb9f32dd968f61bec8cb728007b
nboyers <frenttv@gmail.com>
2025-09-22 11:57:40 -0400

    Updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
