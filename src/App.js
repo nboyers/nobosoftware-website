e72a74510977d71931140dd2e9a813da12d9815d
nboyers <frenttv@gmail.com>
2025-09-22 11:53:48 -0400

    Updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Services from './pages/Services';
-import Contact from './pages/Contact';
-import Footer from './components/Footer';
-import './App.css';
 
-const App = () => {
+function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
+      <h1>Test</h1>
     </div>
   );
-};
+}
 
 export default App;
